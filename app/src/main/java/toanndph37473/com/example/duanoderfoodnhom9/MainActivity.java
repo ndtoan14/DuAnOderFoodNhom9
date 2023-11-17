4b346b6d2e7658589d3c54e56993d4b4ea4363ae
ADMIN <ndtoan14@gmail.com>
2023-11-17 17:10:04 +0700

    toan

---
+++ app/src/main/java/toanndph37473/com/example/duanoderfoodnhom9/MainActivity.java
@@ -0,0 +1,15 @@
+package toanndph37473.com.example.duanoderfoodnhom9;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
